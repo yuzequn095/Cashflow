f73bc84a7b90ee6e976c7c9667c00e8b49e40a5e
yuzequn095 <yuzequn095@gmail.com>
2023-04-27 17:01:43 -0700

    Adds line chart

--- Cashflow/ContentView.swift
+++ Cashflow/ContentView.swift
@@ -6,8 +6,12 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct ContentView: View {
+    
+    @EnvironmentObject var transactionListVM: TransactionListViewModel
+    
     var body: some View {
         NavigationView {
             ScrollView {
@@ -17,6 +21,25 @@ struct ContentView: View {
                         .font(.title2)
                         .bold()
                     
+                    // Chart
+                    let data = transactionListVM.accumulateTransactions()
+                    
+                    if !data.isEmpty {
+                        
+                        let totalExpenses = data.last?.1 ?? 0
+                        
+                        CardView {
+                            VStack(alignment: .leading) {
+                                ChartLabel(totalExpenses.formatted(.currency(code: "USD")), type: .title, format: "$%.02f")
+                                LineChart()
+                            }
+                            .background(Color.systemBackground)
+                        }
+                        .data(data)
+                        .chartStyle(ChartStyle(backgroundColor: Color.systemBackground,
+                                               foregroundColor: ColorGradient(Color.icon.opacity(0.4), Color.icon)))
+                        .frame(height: 300)
+                    }
                     // Transacion List
                     RecentTransactionList()
                 }
--- Cashflow/Extensions.swift
+++ Cashflow/Extensions.swift
@@ -31,3 +31,15 @@ extension String {
         return parseDate
     }
 }
+
+extension Date: Strideable {
+    func formatted() -> String {
+        return self.formatted(.dateTime.year().month().day())
+    }
+}
+
+extension Double {
+    func roundedTo2Digits() -> Double {
+        return (self * 100).rounded() / 100
+    }
+}
